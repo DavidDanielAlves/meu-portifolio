98412e54ba4e44fbbb9869928766e9864f16c0c0
DavidDanielAlves <140220407+DavidDanielAlves@users.noreply.github.com>
2023-12-27 23:03:53 -0300

    Add files via upload

---
+++ src/scripts/about.js
@@ -0,0 +1,7 @@
+import { mudarBotaoChangeBackground } from "../scripts/mudarBackground.js";
+import { inputChangeBackground } from "../scripts/mudarBackground.js";
+import { abrirFecharMenu } from "../scripts/fecharEAbrirMenu.js";
+
+inputChangeBackground.addEventListener('change', mudarBotaoChangeBackground);
+
+abrirFecharMenu();
