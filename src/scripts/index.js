44b61e13ff64fb80f349b2cc4463896b7bd716bf
DavidDanielAlves <140220407+DavidDanielAlves@users.noreply.github.com>
2023-12-29 12:19:04 -0300

    Add files via upload

--- src/scripts/index.js
+++ src/scripts/index.js
@@ -3,16 +3,10 @@ import { mudarBotaoChangeBackground } from '../scripts/mudarBackground.js';
 import { abrirFecharMenu } from '../scripts/fecharEAbrirMenu.js';
 import { animationQuandoEstavisivel } from '../scripts/animarElementosObservaveis.js';
 
-const containerAbout = document.querySelector('.container-about');
-const tituloPrincipal = document.querySelector('h1');
 const tituloRedes = document.querySelector('.container-redes h2');
-const containerConteudo = document.querySelector('.container-texto-seta');
 
 inputChangeBackground.addEventListener('change', mudarBotaoChangeBackground);
 
 abrirFecharMenu();
 
-animationQuandoEstavisivel(containerAbout, 'animationConteudoEntrandoNaTela');
-animationQuandoEstavisivel(tituloPrincipal, 'animationPularTexto');
 animationQuandoEstavisivel(tituloRedes, 'animationPularTexto');
-animationQuandoEstavisivel(containerConteudo, 'animationConteudoEntrandoNaTela');
